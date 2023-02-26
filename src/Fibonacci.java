3b0bc88e864820051916e16e46a7f974b688b246
PAIVANicolas-3il <75265504+PAIVANicolas@users.noreply.github.com>
2023-02-26 22:04:37 +0100

    Ajoue de la classe Fibonacci qui implement Iterable

---
+++ src/Fibonacci.java
@@ -0,0 +1,10 @@
+import java.net.Inet4Address;
+import java.util.Iterator;
+
+public class Fibonacci implements Iterable<Integer>{
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return null;
+    }
+}
