d6ec47d7c77951de7c699b3c1efedaaad7bfec3e
PAIVANicolas-3il <75265504+PAIVANicolas@users.noreply.github.com>
2023-02-26 23:03:27 +0100

    Exercice fini

--- src/TestFiboIterator.java
+++ src/TestFiboIterator.java
@@ -119,4 +119,23 @@ public class TestFiboIterator {
         }
         assertEquals(resultat,55);
     }
+
+    @Test
+    public void TestFiboIteratorNext30 (){
+        int resultat = 0;
+
+        int tableau[] = {0,1, 1, 2, 3, 5, 8, 13, 21, 34,
+                55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181,
+                6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229};
+
+        for (int z =0; z<tableau.length;z++) {
+            FiboIterator fiboIterator = new FiboIterator(z);
+            while (fiboIterator.hasNext()) {
+                resultat = fiboIterator.next();
+
+            }
+            assertEquals(resultat, tableau[z]);
+        }
+
+    }
 }
