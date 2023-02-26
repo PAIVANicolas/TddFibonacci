ba79eabdcfb0348900ab788709aa47ddfb7f2d4a
PAIVANicolas-3il <75265504+PAIVANicolas@users.noreply.github.com>
2023-02-26 22:07:57 +0100

    Ajoue d'une variable rang et modification de hasNext

--- src/FiboIterator.java
+++ src/FiboIterator.java
@@ -3,14 +3,17 @@ import java.util.Iterator;
 public class FiboIterator implements Iterator<Integer> {
 
     private int valeurInserer;
+    private int rang;
+
 
     public FiboIterator(int valeurInserer) {
         this.valeurInserer = valeurInserer;
+        this.rang = 0;
     }
 
     @Override
     public boolean hasNext() {
-        return false;
+        return rang < valeurInserer;
     }
 
     @Override
