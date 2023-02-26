d0d03e0cae62cfef14e5f2617af2825c2852a60e
PAIVANicolas-3il <75265504+PAIVANicolas@users.noreply.github.com>
2023-02-26 22:02:03 +0100

    Ajoue du test TestFiboIteratorHasNextTrue qui bloque

--- src/TestFiboIterator.java
+++ src/TestFiboIterator.java
@@ -1,13 +1,20 @@
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class TestFiboIterator {
 
     @Test
-    public void TestFiboIteratorHasNext (){
+    public void TestFiboIteratorHasNextFalse (){
         FiboIterator fiboIterator = new FiboIterator(0);
         assertFalse(fiboIterator.hasNext());
+    }
+
+    @Test
+    public void TestFiboIteratorHasNextTrue (){
+        FiboIterator fiboIterator = new FiboIterator(1);
+        assertTrue(fiboIterator.hasNext());
     }
 
 }
